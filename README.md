4e6e2894be52f31657707cdff12b99f1f2ab9b0a
klyed <kylejrturnbull@gmail.com>
2021-05-05 03:26:13 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # hbd.funder-sniper
-A small / light weight NodeJS utility bot capable of monitoring the HIVE blockchain with minimal user input required, focusing solely on supporting the HBD Stabilizer experiment account @hbd.funder's daily comments and upon detection voting for them close to the optimal time for maximum curation rewards.
+A small / light weight NodeJS utility bot capable of monitoring the HIVE blockchain with minimal user input required.
+Focusing solely on supporting the HBD Stabilizer experiment account hbd.funder daily comments
+Upon detection the software will invoke auto voting for them close to the optimal time for maximum curation rewards.
+By autovoting hbd.funder account not only will you get *thicc* curation rewards, but you'll also help procure developer DAO funding!
+
+This is a NodeJS application and thus requires NodeJS installed, as well as an initial "npm install" in the directory it's placed.
+If you need to download NodeJS, here is a link: https://nodejs.org/en/download/ - download the appropriate version for your Operating System
+Once you have that installed and this folder downloaded remember to "npm install" in Powershell, Windows CMD or whatever Bash or Linux distro you like.
+
+
+#**CONFIG**
+Please put your HIVE account name and private posting key into the .env.example config file then remove the ".example" part at the end:
+
+```
+HERO=yourusernamehere
+HERO_WIF_KEY=yourpostingprivatekeyhere
+```
+
+the final environmental / config file should only be named ".env" and nothing else. Once that is done run the script with "node sniper.js".
+If you wish to close the program for whatever reason use ctrl+c in your console to halt the application. It saves no data and requires only the ".env" config filled in.
+
+*Note: A small 1% weight vote is also applied to the developers comments and posts, you can opt out by removing my name from the list. :)*
+*This is mostly done just so I can see who all is utilizing this script to maximize their curation rewards and not so much for monetary gain*
+
+
+##TL;DR:
+Install NodeJS
+Download this app from github.com/klyed/hbd.funder-sniper
+Open download location in console and type "npm install"
+Fill in the ".env" file with your username and private posting key
+Start the bot up by typing "node sniper.js" in console
+???
+Profit
---
+++ sniper.js
@@ -0,0 +1,160 @@
+//Remember to install NodeJS and run 'npm install' to install dependencies
+//hbd.funder-sniper v0.0.3 - coded by @KLYE - PeakD.com/@klye - github.com/klyed
+
+//This applet is intended to help users automate their votes on the 10 daily comments of the @hbd.funder account
+//It will automatically detect the target account has created a comment and set a vote to go off on a delay
+//With the @hbd.funder account creating 10 comments a day that recieve heavy upvotes, this is a great way to earn curation rewards
+//You may also use this code to monitor for comments from multiple accounts and upvote said comments
+
+//NOTE: This code is overly commented in order to help non-tech types to understand what is going on and why
+//This applet is an evolution of a downvote / blockchain warfare script I wrote back in the Steem era
+//While it does serve it's purpose, it's not terribly well optimized nor 100% stable at times, keep that in mind
+
+//Lets declare the required dependencies for the applet below...
+
+//used to monitor and interact with the HIVE blockchain
+var hivejs = require("@hiveio/hive-js");
+//used to generate timestamps
+var dateTime = require('node-datetime');
+//used to allow for .env config file to hold username / keys
+const dotenv = require('dotenv');
+//create a date to display on startup
+var dt = dateTime.create();
+
+//bring the .env config file holding users account and private posting key
+dotenv.config();
+
+//fetch the deploying users account name
+var hero = process.env.HERO;
+//fetch the deploying users private posting key
+var herowifkey = process.env.HERO_WIF_KEY;
+
+// ***IMPORTANT*** enter target accounts here you wish to monitor for comments (No @)
+//(feel free to remove the klye entry, the script is set up by default to vote a 1% weight on my posts / comments. Mainly to help me see who is using this)
+var targetaccts = ["hbd.funder", "klye"];
+
+//declare some basic counting variables and specify the starting index of the HIVE API nodes
+var totalblocks = 0;
+var totalvote = 0;
+var pendingvote = 0;
+var totalcomment = 0;
+var totalops = 0;
+var apiindex = 1;
+var opgrab;
+var voteweight = 10000; //10000 equals 100% vote strength
+
+//declare the amount of time in milliseconds to delay your vote after comment is detected
+var votedelay = 300000; //300000 milliseconds is 5 minutes
+
+//a list of public RPC nodes used to monitor and broadcast to HIVE
+const apinodes = ["hived.privex.io", "api.hivekings.com", "api.deathwing.me", "api.hive.blog", "api.openhive.network", "hive.loelandp.nl", "hive-api.arcange.eu", "rpc.ausbit.dev", "anyx.io"];
+
+//set hivekings.com as our HIVE API node
+hivejs.api.setOptions({ url: "https://api.hivekings.com" });
+
+//create a function that can cycle between different HIVE API nodes when function called
+async function changenode() {
+  if (apiindex < apinodes.length){//if the apiindex variable is less than the length of the API node list
+    //increase the apiindex variable by 1
+    apiindex++;
+  } else if (apiindex == apinodes.length) {//if apiindex is at the end of the list
+    //reset the apiindex variable back to the beginning of the list
+    apiindex = 0;
+  }//END else if (apiindex == apinodes.length)
+  //update the HIVE API node we are using
+  await hivejs.api.setOptions({url:`https://${apinodes[apiindex]}`});
+  //show that the HIVE API node has been changed
+  console.log(`CHAIN: Changed API Node to ${apinodes[apiindex]}`);
+};//END async function changenode()
+
+  //get the current date to display at start
+  var starttime = dt.format('Y-m-d H:M:S');
+
+  //display what this applet is and who made it
+  console.log("HBD.Funder Post Sniper v0.0.3 - By @KLYE");
+  //display the start time of the applet
+  console.log("Start Time: " + starttime + "\n");
+
+  //define a function variable used to check if a comment operation is from our target
+  var process_comment = function(op) {
+    //check if the comment operation author is our target account
+    if (targetaccts.includes(op["author"])) {
+      //increase the pendingvote variable by 1
+      pendingvote++;
+      //display that a comment from the targetr account has been detected
+      console.log("-------------------------------------------------------------");
+      console.log(`--HBD.FUNDER COMMENT - Deploying a Vote in ~${(votedelay / 60000).toFixed(1)} Mins! ----`);
+      console.log("-------------------------------------------------------------");
+      //checks to see if the author is KLYE
+      if(op["author"] == "klye") {
+        //if it is KLYE. Set vote strength to 1%
+        voteweight = 100;
+      } else { //else if it's not KLYE
+        //set vote strength to 100%
+        voteweight = 10000;
+      }
+      //after a comment operation from the target accounts has been detected, schedule a vote to occur around the "optimal" time
+      setTimeout(function(){
+        //increase the totalvote variable by 1
+        totalvote++
+        //decrease the pendingvote variable by 1
+        pendingvote--;
+        //broadcast a vote on the comment detected
+        hivejs.broadcast.vote(herowifkey, hero, op["author"], op["permlink"], , function(err, result) { //10000 is 100% vote power
+          //display the outcome of the vote on the comment
+          console.log(err, result);
+        });//END hivejs.broadcast.vote
+      //set your delay after the comment is detected in milliseconds, defined above in the 'votedelay' variable
+      }, votedelay);
+    }//END if (op["author"] == asshat)
+  };//END var process_comment = function(op)
+
+  //define a function to fetch the specified block number dictated by the hivejs.api.streamBlockNumber function below
+  function parseBlock(blockNum) {
+    //fetch the block by number and download it's contents and operations
+    hivejs.api.getBlock(blockNum, function(err, block) {
+      //if the call errors out, show an error message and change node
+      if(err){
+        console.log(`Error Occured: ${err}`);
+        return changenode();
+      }//END if(err)
+      //if the error isn't null valued, display it in console
+      if (err !== null) {
+        console.log(err);
+      };//END if (err !== null)
+      //if we get the block data back
+      if(block){
+        //iterate through the transactions of the block
+        block.transactions.forEach((transaction) => {
+          //iterate through the operations within the transactions
+          transaction.operations.forEach((op) => {
+            //increase the totalops variable by 1
+            totalops++;
+            //grab the type of operation and set the opType variable
+            var opType = op[0];
+            //check if the operation type is that of a comment
+            if (opType == "comment") {
+              //use data of the operation in question into the 'opgrab' variable
+              opgrab = op[1];
+              //send the data recently specified to the previously declared process_comment function
+              process_comment(opgrab);
+            };//END if (opType == "comment")
+          });//END transaction.operations.forEach((op)
+        });//END block.transactions.forEach((transaction)
+      };//END if(block)
+    });//END hivejs.api.getBlock(blockNum, function(err, block)
+  };//END function parseBlock(blockNum)
+
+  //stream the latest block number on HIVE network
+  hivejs.api.streamBlockNumber(async function(err1, newestblock) {
+    //if the stream fails for some reason or errors, change the HIVE API node in use
+    if(err1) changenode();
+    //if the newest block integer is returned, send this number to the parseBlock function above to parse it's contents
+    if(newestblock) await parseBlock(newestblock);
+    //increase the totalblocks variable by 1
+    totalblocks++
+    //print out the stats on what block we're on, how many blocks and operations we've parsed and also display how many pending and completed votes exist
+    process.stdout.write(`Target(s): ${targetaccts} - #${newestblock} - Blocks Monitored: ${totalblocks} - Operations: ${totalops} - Pending: ${pendingvote} - Completed: ${totalvote}`);
+    //move the console cursor back to the start to avoid writing more lines than necesarry
+    process.stdout.cursorTo(0);
+  });//END hivejs.api.streamBlockNumber(async function(err1, newestblock)
